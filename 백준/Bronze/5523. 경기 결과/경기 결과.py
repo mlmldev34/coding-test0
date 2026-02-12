f28bc076d6c83beb3ba34c2350ea1e8c63cd90e1
MLDEV <100326828+mlmldev34@users.noreply.github.com>
2026-02-12 23:25:52 +0900

    [Bronze III] Title: 경기 결과, Time: 1036 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/5523. 경기 결과/경기 결과.py
+++ 백준/Bronze/5523. 경기 결과/경기 결과.py
@@ -1,8 +1,12 @@
-n=int(input())
+import sys
+
+input = sys.stdin.readline
+
+n=int(input().strip())
 r1=0
 r2=0
 for _ in range(n):
-    a,b=map(int,input().split())
+    a,b=map(int,input().strip().split())
     r1+=int(a>b)
     r2+=int(b>a)
 print(r1,r2)
